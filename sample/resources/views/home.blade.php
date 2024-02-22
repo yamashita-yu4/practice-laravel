30e90b6ce2b3d2bad865de9647794741c0f6541d
山下優 <yu.yamashita@ponion.jp>
2024-02-22 13:42:41 +0900

    ホーム画面の作成/ルーティング変更

---
+++ sample/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
+    <title>ホーム画面</title>
+</head>
+<body>
+    <h2 class="title">ようこそ</h2>
+@if (Auth::check())
+    <p class="user">{{ \Auth::user()->name }} さん</p>
+    <p><a href="/logout" class="example">ログアウト</a></p>
+@else
+    <p class="user_guest">ゲストさん</p>
+    <p><a href="/login" class="example">ログイン</a><br>
+    <a href="/register" class="example" >会員登録</a></p>
+@endif
+</body>
+</html>
