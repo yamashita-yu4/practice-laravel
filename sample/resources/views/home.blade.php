f15ad9460c84ea77095ddffa64b46259fb3ca0c8
山下優 <yu.yamashita@ponion.jp>
2024-03-05 13:45:19 +0900

    ホーム画面の修正

--- sample/resources/views/home.blade.php
+++ sample/resources/views/home.blade.php
@@ -8,15 +8,16 @@
 </head>
 <body>
     <h2 class="title">ようこそ</h2>
-@if (Auth::check())
+@Auth
     <p class="user">{{ \Auth::user()->name }} さん</p>
-    <p><a href="/logout" class="example">ログアウト</a></p>
-    <p><a href="/change" class="example">編集</a>
-    <p><a href="/edit" class="example">投稿</a>
-@else
+    <p><a href="{{ route('logout') }}" class="example">ログアウト</a></p>
+    <p><a href="{{ route('change') }}" class="example">編集</a>
+    <p><a href="{{ route('edit.index') }}" class="example">投稿</a>
+@endAuth
+@Guest
     <p class="user_guest">ゲストさん</p>
-    <p><a href="/login" class="example">ログイン</a><br>
-    <a href="/register" class="example" >会員登録</a></p>
-@endif
+    <p><a href="{{ route('login') }}" class="example">ログイン</a><br>
+    <a href="{{ route('register') }}" class="example" >会員登録</a></p>
+@endGuest
 </body>
 </html>
